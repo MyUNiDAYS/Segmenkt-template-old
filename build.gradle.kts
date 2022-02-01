7a456c851d260d0ff00003a7548823b231c19525
Andrew Reed <andrew_reed@hotmail.com>
2022-02-01 19:11:56 +0000

    fixed issue with library publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,13 @@ plugins {
     kotlin("plugin.serialization") version "1.6.0"
     id("com.android.library") version "7.1.0"
     id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    `maven-publish`
 }
 
 group = "com.myunidays"
 version = "0.0.1"
 
-val name = "segmenkt"
+val frameworkName = "segmenkt"
 val ktor_version = "1.6.6"
 val coroutines_version = "1.6.0-native-mt"
 val serialization_version = "1.3.0"
@@ -23,10 +24,10 @@ kotlin {
         publishAllLibraryVariants()
     }
     iosSimulatorArm64 {
-        binaries.framework(name)
+        binaries.framework(frameworkName)
     }
     iosArm64("ios") {
-        binaries.framework(name)
+        binaries.framework(frameworkName)
     }
     sourceSets {
         val commonMain by getting {
@@ -80,18 +81,18 @@ android {
 
 kotlin {
     tasks {
-        val capitalizedName = name.capitalize()
+        val capitalizedName = frameworkName.capitalize()
         register("copyIosFrameworkIntoUniversalFramework", Copy::class) {
-            from(file("$buildDir/bin/ios/${name}ReleaseFramework/$name.framework"))
-            into(file("$buildDir/universal/$name.xcframework/ios-arm64/$name.framework"))
+            from(file("$buildDir/bin/ios/${frameworkName}ReleaseFramework/$frameworkName.framework"))
+            into(file("$buildDir/universal/$frameworkName.xcframework/ios-arm64/$frameworkName.framework"))
         }
         register("copySimulatorFrameworkIntoUniversalFramework", Copy::class) {
-            from(file("$buildDir/bin/iosSimulatorArm64/${name}ReleaseFramework/$name.framework"))
-            into(file("$buildDir/universal/$name.xcframework/ios-arm64_x86_64-simulator/$name.framework"))
+            from(file("$buildDir/bin/iosSimulatorArm64/${frameworkName}ReleaseFramework/$frameworkName.framework"))
+            into(file("$buildDir/universal/$frameworkName.xcframework/ios-arm64_x86_64-simulator/$frameworkName.framework"))
         }
         register("copyPlistIntoUniversalFramework", Copy::class) {
             from(file("${project.projectDir}/Info.plist"))
-            into(file("$buildDir/universal/$name.xcframework"))
+            into(file("$buildDir/universal/$frameworkName.xcframework"))
         }
         register("copyFrameworksIntoUniversalFramework") {
             mustRunAfter("link${capitalizedName}Ios", "link${capitalizedName}IosSimulatorArm64")
